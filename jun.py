80244467c2049d2cecb2301d64030e64783936de
Tomás <tomasafonsoferreiracarvalho@gmail.com>
2025-12-09 09:22:14 +0000

    junc.py

---
+++ jun.py
@@ -0,0 +1,7 @@
+fruta_fav_tom = {'banana', 'maça', 'uva', 'perâ'}
+fruta_fav_amigo = {'banana', 'laranja', 'abacate', 'fruta dragão', 'uva'}
+union = fruta_fav_tom.union(fruta_fav_amigo)
+inter = fruta_fav_tom.intersection(fruta_fav_amigo)
+print(union)
+print(inter)
+
