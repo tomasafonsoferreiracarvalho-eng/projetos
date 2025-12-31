fa1ea3f5f725b7d677e041feb262899fa9147ec4
Tomás <tomasafonsoferreiracarvalho@gmail.com>
2025-12-31 18:07:34 +0000

    ex3.py

---
+++ ex3.py
@@ -0,0 +1,8 @@
+import random
+print('Resposta ao teu Euro-milhões')
+print('-'*30)
+for i in range(5):
+    print('Números: ',random.randint(1,51))
+print('-'*30)
+for s in range(2):
+    print('Estrelas: ',random.randint(1,12))
